e56b5c7cb0768a649e5331962c259e589392f809
Joel Razuri <jrazuri98@hotmail.com>
2024-05-06 17:19:28 -0300

    DETAIL, UPDATE y DELETE view

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from apps.users.api.views import UserAPIView
-from apps.users.api.views import user_api_view
+from apps.users.api.views import user_api_view, user_detail_api_view
 
 urlpatterns = [
     # path('', UserAPIView.as_view(), name='usuario_api'),
     path('', user_api_view, name='usuario_api'),
+    path('<int:pk>', user_detail_api_view, name='usuario_api_detail'),
 ]
--- apps/users/api/views.py
+++ apps/users/api/views.py
@@ -19,9 +19,32 @@ def user_api_view(request):
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(users_serializer.data)
+    
     elif request.method == 'POST':
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
             return Response(user_serializer.data, status=201)
         return Response(user_serializer.errors, status=400)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=404)
+    
+    if request.method == 'GET':
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+    
+    elif request.method == 'PUT':
+        user_serializer = UserSerializer(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        return Response(user_serializer.errors, status=400)
+    
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=204)
